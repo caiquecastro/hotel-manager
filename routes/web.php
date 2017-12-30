0a162c2f941dbf346fc2b02c9c265a983ec0fdb5
Caique de Castro <caiquecastro@outlook.com>
2017-12-30 16:46:17 -0200

    fix: Rename route for dashboard

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', 'HomeController@index');
-
-Route::get('dashboard', 'DashboardController@index');
+Route::get('/', 'DashboardController@index');
 
 Route::resource('customers', 'CustomersController');
 
