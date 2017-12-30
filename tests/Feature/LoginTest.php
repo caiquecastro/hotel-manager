5ccb0e10d26625179c2fdde13eda4fa5b02b7d45
Caique de Castro <caiquecastro@outlook.com>
2017-12-30 17:26:11 -0200

    fix: apply fixes from style ci
    Remove useless import.
    Refresh database on Login Test.

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -3,11 +3,12 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LoginTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testUserCanLogin()
     {
         $this->withoutExceptionHandling();
