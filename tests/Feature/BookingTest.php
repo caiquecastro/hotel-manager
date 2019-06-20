21f78ad2976e0309aa318e21bbaae779cc8ff555
Caique de Castro <caiquecastro@outlook.com>
2019-06-20 13:57:07 -0300

    Add test to forbid conflicting bookings

--- tests/Feature/BookingTest.php
+++ tests/Feature/BookingTest.php
@@ -18,4 +18,28 @@ class BookingTest extends TestCase
 
         $response->assertSuccessful();
     }
+
+    public function testForbidConflictingBookings()
+    {
+        $this->withoutExceptionHandling();
+
+        $room = factory(\App\Room::class)->create();
+        factory(\App\Booking::class)->create([
+            'room_id' => $room->id,
+            'checkin' => '04/03/2019',
+            'checkout' => '05/04/2019',
+        ]);
+
+        $this->loginUser();
+        $this->post('bookings', [
+            'customer_id' => factory(\App\Customer::class)->create()->id,
+            'room_id' => $room->id,
+            'checkin' => '04/03/2019',
+            'checkout' => '26/04/2019',
+        ]);
+
+        $bookingsCount = \App\Booking::count();
+
+        $this->assertEquals(1, $bookingsCount);
+    }
 }
