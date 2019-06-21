fa0fe4ea74197059652a5890d0ceaafa0add2a53
Caique de Castro <caiquecastro@outlook.com>
2019-06-21 12:04:40 -0300

    Fix style for error list message

--- resources/views/errors/list.blade.php
+++ resources/views/errors/list.blade.php
@@ -1,5 +1,5 @@
 @if ($errors->any())
-    <ul class="alert alert-danger list-unstyled">
+    <ul class="notification is-danger list-unstyled Notification">
         @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
         @endforeach
