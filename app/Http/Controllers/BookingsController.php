b348969751b43978dcf63a086ffe5d072aeb4eff
Caique de Castro <caiquecastro@outlook.com>
2019-06-22 08:17:56 -0300

    Add auth middleware on missing controllers

--- app/Http/Controllers/BookingsController.php
+++ app/Http/Controllers/BookingsController.php
@@ -11,6 +11,11 @@ use App\Http\Requests\BookingRequest;
 
 class BookingsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class ReportsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
