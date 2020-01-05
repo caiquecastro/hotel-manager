e908412661261ee15c96137febd8e6e762444217
Caique Castro <castro.caique@gmail.com>
2020-01-05 10:13:39 -0300

    Redirect home page to bookings page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('/', 'DashboardController@index');
+Route::redirect('/', 'bookings');
 
 Route::resource('customers', 'CustomersController');
 
