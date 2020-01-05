5c067e384e537c7f219c3c445f297097058d81fb
Caique Castro <castro.caique@gmail.com>
2020-01-05 10:22:31 -0300

    Fix status for room

--- app/Room.php
+++ app/Room.php
@@ -8,6 +8,17 @@ class Room extends Model
 {
     protected $fillable = ['number', 'floor', 'type_id'];
 
+    public function getStatusAttribute($value)
+    {
+        $now = now();
+        $hasBooking = $this->bookings()
+            ->where('checkin', '<=', $now)
+            ->where('checkout', '>=', $now)
+            ->count();
+
+        return $hasBooking ? 'occupied' : $value;
+    }
+
     public function features()
     {
         return $this->belongsToMany('App\Feature');
