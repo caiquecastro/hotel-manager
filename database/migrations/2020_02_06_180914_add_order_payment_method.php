1ed1417869796840e33f23c9eba208f327fa4aeb
Caique Castro <castro.caique@gmail.com>
2020-02-06 18:13:01 -0300

    feature: Add migration to add payment method column on orders table

---
+++ database/migrations/2020_02_06_180914_add_order_payment_method.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOrderPaymentMethod extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->enum('payment_method', [
+                'cash',
+                'debitCard',
+                'creditCard',
+            ])->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
