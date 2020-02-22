8c4666da1bb7d938e48abf5cb0ca30661e7e9bf2
Caique Castro <castro.caique@gmail.com>
2020-02-22 15:14:15 -0300

    fix: Fix lint

--- app/Http/Controllers/OrderItemsController.php
+++ app/Http/Controllers/OrderItemsController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OrderRequest;
 use App\Order;
 use App\Product;
-use App\Http\Requests\OrderRequest;
 
 class OrderItemsController extends Controller
 {
