1d4595e5fc1b745f1e964a746a1db5326f0bc2c9
Caíque de Castro Soares da Silva <castro.caique@gmail.com>
2020-02-22 15:40:09 -0300

    fix: Adjust endpoint to create order items (#207)

--- app/Http/Controllers/OrderItemsController.php
+++ app/Http/Controllers/OrderItemsController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OrderRequest;
+use App\Order;
+use App\Product;
+
 class OrderItemsController extends Controller
 {
     public function create()
     {
         return view('order-items.create');
+    }
+
+    public function store(Order $order, OrderRequest $request)
+    {
+        $product = Product::findOrFail($request->input('product_id'));
+        $amount = $request->input('amount');
+
+        return $order->items()->create([
+            'product_id' => $product->id,
+            'price' => $product->price,
+            'amount' => $amount,
+        ]);
     }
 }
