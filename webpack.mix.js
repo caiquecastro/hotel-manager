ee6e26d3a3309e49f9d17555e25225392a3f300b
Caique Silva <caique.castro@venturus.org.br>
2020-12-27 19:28:12 -0300

    feat: Add menu page

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
+   //.copy('resources/assets/pdf/menu.pdf', 'public/assets/pdf/menu.pdf')
    .version();
