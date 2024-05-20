1a0866f567364bda66e3b0150cd7807b1b532e68
Caíque de Castro Soares da Silva <castro.caique@gmail.com>
2024-05-20 20:26:30 -0300

    style: fix for styleci

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -42,8 +42,8 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name'     => 'required|max:255',
-            'email'    => 'required|email|max:255|unique:users',
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
             'password' => 'required|confirmed|min:6',
         ]);
     }
