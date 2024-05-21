dc125443e2710e1ebff805c01c7f50dd674ba7f3
Caíque de Castro Soares da Silva <castro.caique@gmail.com>
2024-05-20 21:04:33 -0300

    refactor: use laravel-html helpers to build form

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <h1>Quartos</h1>
-    <a href="{{ action('RoomsController@create') }}" class="btn btn-primary mb-3">Adicionar novo</a>
-    <a href="{{ action('FeaturesController@index') }}" class="btn btn-primary mb-3">Características</a>
-    <a href="{{ action('TypesController@index') }}" class="btn btn-primary mb-3">Tipos de Quarto</a>
+    <a href="{{ url('/rooms/create') }}" class="btn btn-primary mb-3">Adicionar novo</a>
+    <a href="{{ url('/features') }}" class="btn btn-primary mb-3">Características</a>
+    <a href="{{ url('/types') }}" class="btn btn-primary mb-3">Tipos de Quarto</a>
     @include('errors.list')
     @include('partials._messages')
     <table class="table">
@@ -50,7 +50,7 @@
                     >
                         <span class="fa fa-eye"></span>
                     </a>
-                    {!! Form::open(['method'=>'PUT', 'action'=>['RoomsController@putMaintenance', $room->id], 'class' => 'display-inline-block']) !!}
+                    {{ html()->form('PUT', '/rooms/' . $room->id . '/maintenance')->class('display-inline-block')->open() }}
                         <button
                             class="btn btn-warning btn-sm"
                             type="submit"
@@ -59,7 +59,7 @@
                         >
                             <span class="fa fa-wrench"></span>
                         </button>
-                    {!! Form::close() !!}
+                    {{ html()->form()->close() }}
                     <a
                         href="{{ action('BookingsController@create', ['roomId' => $room->id]) }}"
                         class="btn btn-primary btn-sm"
@@ -76,7 +76,7 @@
                     >
                         <span class="fa fa-pencil"></span>
                     </a>
-                    {!! Form::open(['method'=>'DELETE', 'action'=>['RoomsController@destroy', $room->id], 'class' => 'display-inline-block']) !!}
+                    {{ html()->form('DELETE', '/rooms/' . $room->id)->class('display-inline-block')->open() }}
                         <button
                             class="btn btn-danger btn-sm"
                             type="submit"
@@ -85,7 +85,7 @@
                         >
                             <span class="fa fa-trash"></span>
                         </button>
-                    {!! Form::close() !!}
+                    {{ html()->form()->close() }}
                 </td>
             </tr>
         @endforeach
